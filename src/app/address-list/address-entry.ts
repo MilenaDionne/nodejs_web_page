b53da4c0a25d38097b5d040f76a5d65cb62a0b50
MilenaDionne <33129723+MilenaDionne@users.noreply.github.com>
2020-09-25 18:34:44 -0400

    lab 2 - address addition
    lab 2 Miléna Dionne - 8916596

--- src/app/address-list/address-entry.ts
+++ src/app/address-list/address-entry.ts
@@ -4,12 +4,21 @@ export class AddressEntry {
   public phone?: string;
   public email?: string;
   public notes?: string;
+  public streetadd?: string;
+  public city: string;
+  public country: string;
+  public postalcode?: string;
 
-  constructor(firstName?: string, lastName?: string, phone?: string, email?: string, notes?: string) {
+
+  constructor(firstName?: string, lastName?: string, phone?: string, email?: string, notes?: string, streetadd?: string, city?: string, country?: string, postalcode?: string) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phone = phone;
     this.email = email;
     this.notes = notes;
+    this.streetadd = streetadd;
+    this.city = city;
+    this.country = country;
+    this.postalcode = postalcode;
   }
 }
